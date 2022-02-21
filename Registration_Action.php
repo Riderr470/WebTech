0a1263cc408b7eb4db0ab87721b6e6cedafd8696
Riderr470 <94305015+Riderr470@users.noreply.github.com>
2022-02-21 13:07:46 +0600

    update

--- Registration_Action.php
+++ Registration_Action.php
@@ -9,7 +9,7 @@
 
 	<?php 
 
-		$firstnameErr = $lastnameErr = $gendererr = $religionerr = $PreAddresserr = $emailerr = $dateErr ="";
+		$firstnameErr = $lastnameErr = $gendererr = $religionerr = $PreAddresserr = $emailerr = $dateErr = $passErr = $CpassErr = $UsernameErr = "";
 		$firstname = $lastname = $gender = $religion = $PreAddress =$email = $Username = $password = $Cpassword = $date ="";
 		$isValid = true;
 		$isChecked = false;
@@ -27,8 +27,14 @@
 			$gender = test($_POST['gender']);
 			$religion = test($_POST['religion']);
 			$PreAddress = test($_POST['PreAddress']);
+			$PmAddress = test($_POST['pmtadd']);
+			$Phone = test($_POST['phone']);
 			$email = test($_POST['email']);
+			$Url = test($_POST['Url']);
 			$date = test($_POST['date']);
+			$Username = test($_POST['Uname']);
+			$password = test($_POST['pass']);
+			$Cpassword = test($_POST['Cpass']);
 
 			$isChecked = true;
 			if (empty($firstname)) {
@@ -38,6 +44,14 @@
 			if (empty($lastname)) {
 				$isValid = false;
 				$lastnameErr = "Last name can not be empty";
+			}
+			if (empty($gender)) {
+				$isValid = false;
+				$gendererr = "Gender must be selected";
+			}
+			if (empty($PreAddress)) {
+				$isValid = false;
+				$PreAddresserr = "You must provide present address";
 			}
 			if (empty($email)) {
 				$isValid = false;
@@ -52,12 +66,29 @@
 				$isValid = false;
 				$dateErr = "When is your birthday?";
 			}
+			if (empty($Username)) {
+				$isValid = false;
+				$UsernameErr = "Username can not be empty";
+			}
+			if (empty($password)) {
+				$isValid = false;
+				$passErr = "password can not be empty";
+			}
+			if (empty($Cpassword)) {
+				$isValid = false;
+				$CpassErr = "Confirm password can not be empty";
+			}
+			if($Cpassword!=$password){
+				$isValid = false;
+				$CpassErr = "Password doesn't match with confirm password" ;
+
+			}
 		}
 	?>
 
 	<h2>Registration Form</h2>
 
-	<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+	<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" novalidate>
 		
 	<fieldset>
 		
@@ -76,12 +107,14 @@
 		<br><br>
 
 		<label for="gender">Gender*:</label>
-		<input type="radio" name="gender" id="gender" value="Male" >
+		<input type="radio" name="gender" id="male" value="Male" <?php if($gender=='Male'){ echo "checked=checked";}  ?> >
 		<label for="male">Male</label>
-		<input type="radio" name="gender" id="gender" value="Female" >
+		<input type="radio" name="gender" id="female" value="Female" <?php if($gender=='Female'){ echo "checked=checked";}  ?>>
 		<label for="female">Female</label>
-		<input type="radio" name="gender" id="gender" value="Others" >
+		<input type="radio" name="gender" id="others" value="Others" <?php if($gender=='Others'){ echo "checked=checked";}  ?>>
 		<label for="others">Others</label>
+		<br>
+		<span><?php echo $gendererr; ?></span>
 
 		<br><br>
 
@@ -97,6 +130,7 @@
     	<option value="Hindu">Hindu</option>
     	<option value="Christian">Christian</option>
     	<option value="Budha">Budha</option>
+    	<option value="Others">Others</option>
   		</select>
 
 		<br><br>
@@ -106,18 +140,19 @@
 	<fieldset>
 		<legend>Contact Information</legend>
 
-		<label for="Preaddress">Present address*:</label>
-		<textarea name="Preaddress" id="Preaddress" rows="3" cols="30"></textarea> 
+		<label for="PreAddress">Present address*:</label>
+		<textarea name="PreAddress" id="PreAddress" rows="3" cols="30"><?php echo $PreAddress;?></textarea> 
+		<span><?php echo $PreAddresserr; ?></span>
 
 		<br><br>
 
 		<label for="Pmtadd">Permanent Address:</label>
-		<textarea name="pmtadd" id="pmtadd" rows="3" cols="30"></textarea>
+		<textarea name="pmtadd" id="pmtadd" rows="3" cols="30"><?php echo $PmAddress;?></textarea>
 
 		<br><br>
 
 		<label for="phone">Phone:</label>
-		<input type="tel" name="phone" id="phone" size="30" >
+		<input type="tel" name="phone" id="phone" size="30" value="<?php echo $phone;?>" >
 
 		<br><br>
 
@@ -127,8 +162,8 @@
 
 		<br><br>
 
-		<label for="PUrl">Personal Website link:</label>
-		<input type="url" name="PUrl" id="PUrl" size="30" >
+		<label for="Url">Personal Website link:</label>
+		<input type="url" name="Url" id="Url" size="30" value="<?php echo $Url;?>">
 
 		<br><br>
 
@@ -138,17 +173,20 @@
 		<legend>Credentials</legend>
 
 		<label for="Uname">Username*:</label>
-		<input type="text" name="Uname" id="Uname" size="30">
+		<input type="text" name="Uname" id="Uname" size="30" value="<?php echo $Username;?>">
+		<span><?php echo $UsernameErr; ?></span>
 
 		<br><br>
 
 		<label for="pass">Password*:</label>
-		<input type="password" name="pass" id="pass" size="30">
+		<input type="password" name="pass" id="pass" size="30" value="<?php echo $password;?>">
+		<span><?php echo $passErr; ?></span>
 
 		<br><br>
 
 		<label for="Cpass">Confirm Password*:</label>
-		<input type="password" name="Cpass" id="Cpass" size="30">
+		<input type="password" name="Cpass" id="Cpass" size="30" value="<?php echo $Cpassword;?>">
+		<span><?php echo $CpassErr; ?></span>
 
 		<br><br>
 
@@ -162,13 +200,28 @@
 		if ($isChecked and $isValid) {
 			echo "First Name: " . $firstname;
 			echo "<br><br>";
-			echo "Middle Name: " . $middlename;
-			echo "<br><br>";
 			echo "Last Name: " . $lastname;
+			echo "<br><br>";
+			echo "Gender: " . $gender;
+			echo "<br><br>";
+			echo "Date: " . $date;
+			echo "<br><br>";
+			echo "religion: " . $religion;
+			echo "<br><br>";
+			echo "Present Address: " . $PreAddress;
+			echo "<br><br>";
+			echo "Permanent Address: " . $PmAddress;
+			echo "<br><br>";
+			echo "Phone: " . $Phone;
 			echo "<br><br>";
 			echo "Email: " . $email;
 			echo "<br><br>";
-			echo "Date: " . $date;
+			echo "Personal Website Link: " . $Url;
+			echo "<br><br>";
+			echo "Username: " . $Username;
+			echo "<br><br>";
+			echo "Password: " . $password;
+			echo "<br><br>";
 		}
 	?>
 
